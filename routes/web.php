a8544ad9512a9e9d087a1b482ac5ad94dd17c93c
salomon Alfredo Mena Lazarte <salomon.mena@uab.edu.bo>
2023-08-16 19:31:49 -0400

    Se realizo el cambio de nombre de la ruta de eventos a MostrarEventos

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/eventos', [EventoController::class, 'MostrarEvento']);
+Route::get('/MostrarEventos', [EventoController::class, 'MostrarEvento']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
